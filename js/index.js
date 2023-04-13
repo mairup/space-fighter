5021ab9bcfd8e06fbc0b97447ca5084fedee48ab
Mai <maimairupnik@gmail.com>
2023-04-13 20:47:48 +0200

    added bullets

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 const canvas = document.getElementById("canvas")
 const context = canvas.getContext("2d")
 const ship = document.getElementById("ship")
-let shipSpeed = 20;
+const bulletB = document.getElementById("bulletB")
+let shipSpeed = 30
+
+let bullet = {
+    speed: 10,
+    rof: 50,
+    size: 10
+}
+
+let activeBullets = []
 
 let shipPos = {
     x: 100,
@@ -15,7 +24,8 @@ let mousePos = {
 
 let drawInterval = setInterval(() => {
     drawShip()
-    moveShip();
+    moveShip()
+    drawBullets()
 }, 10)
 
 function drawShip() {
@@ -24,20 +34,41 @@ function drawShip() {
 }
 
 canvas.addEventListener("mousemove", (e) => {
-    mousePos = {
-        x: (1000 / canvas.offsetWidth) * e.offsetX,
-        y: (1000 / canvas.offsetHeight) * e.offsetY
-    }
+    mousePos.x = (1000 / canvas.offsetWidth) * e.offsetX
+    mousePos.y = (1000 / canvas.offsetHeight) * e.offsetY
+})
+
+canvas.addEventListener("click", () => {
+    fire()
 })
 
 function moveShip() {
-    if ((mousePos.x - shipPos.x) < 30)
-        shipPos.x += Math.ceil((mousePos.x - shipPos.x) / (shipSpeed / 2))
-    else
-        shipPos.x += Math.ceil((mousePos.x - shipPos.x) / shipSpeed)
+    if (Math.sqrt(Math.abs((mousePos.x - shipPos.x) * (mousePos.x - shipPos.x) - ((mousePos.y - shipPos.y) * (mousePos.y - shipPos.y)))) < 100) {
+        shipPos.x += (mousePos.x - shipPos.x) / (shipSpeed / 2)
+        shipPos.y += (mousePos.y - shipPos.y) / (shipSpeed / 2)
+    }
 
-    if ((mousePos.y - shipPos.y) < 30)
-        shipPos.y += Math.ceil((mousePos.y - shipPos.y) / (shipSpeed / 2))
-    else
-        shipPos.y += Math.ceil((mousePos.y - shipPos.y) / shipSpeed)
+    else {
+        shipPos.x += (mousePos.x - shipPos.x) / shipSpeed
+        shipPos.y += (mousePos.y - shipPos.y) / shipSpeed
+    }
+
+}
+
+function fire() {
+    activeBullets.push({
+        speed: bullet.speed,
+        size: bullet.size,
+        x: shipPos.x,
+        y: shipPos.y - 70
+    })
+}
+
+function drawBullets() {
+    for (let i = 0; i < activeBullets.length; i++) {
+        context.drawImage(bulletB, activeBullets[i].x - 75, activeBullets[i].y - 75, 150, 150)
+        activeBullets[i].y -= activeBullets[i].speed
+        if (activeBullets[i].y < -100)
+            activeBullets.splice(i, 1)
+    }
 }
